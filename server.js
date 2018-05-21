42f0784717392bf51bce0e0db3009b3d9416b74d
Maddy Dannenberg <mmdberg@gmail.com>
2018-05-21 09:38:25 -0600

    Set up server.js and display static page

---
+++ server.js
@@ -0,0 +1,13 @@
+const express = require('express');
+const app = express();
+const bodyParser = require('body-parser');
+
+app.set('port', process.env.PORT || 3000);
+app.locals.title = 'Photo Tracker Keeper';
+
+app.use(bodyParser.json());
+app.use(express.static('public'));
+
+app.listen(app.get('port'), () => {
+  console.log(`${app.locals.title} is running on ${app.get('port')}`)
+})
