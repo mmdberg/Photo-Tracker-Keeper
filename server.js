d27c082f0dce76380d10b0fbc9c58bce302b6940
Maddy Dannenberg <mmdberg@gmail.com>
2018-05-21 10:09:35 -0600

    Add get endpoint to server.js

--- server.js
+++ server.js
@@ -2,11 +2,25 @@ const express = require('express');
 const app = express();
 const bodyParser = require('body-parser');
 
+const environment = process.env.NODE_ENV || 'development';
+const configuration = require('./knexfile')[environment];
+const database = require('knex')(configuration);
+
 app.set('port', process.env.PORT || 3000);
 app.locals.title = 'Photo Tracker Keeper';
 
 app.use(bodyParser.json());
 app.use(express.static('public'));
+
+app.get('/api/v1/photos', (request, response) => {
+  database('photos').select()
+    .then((photos) => {
+      response.status(200).json(photos);
+    })
+    .catch((error) => {
+      response.status(500).json({error})
+    })
+})
 
 app.listen(app.get('port'), () => {
   console.log(`${app.locals.title} is running on ${app.get('port')}`)
